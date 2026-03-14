b34efe6de9e57e362ec74a4b5e36706f262978c6
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-14 22:07:51 +0545

    added a loop in the calculator to enable continuous use of the user

--- calculator.py
+++ calculator.py
@@ -15,22 +15,33 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+while True:
+    choice = int(input("Enter what you want to do 1-4: "))
+    a = int(input("Enter the value of a: "))
+    b = int(input("Enter the value of b: "))
 
-choice = int(input("Enter what you want to do 1-4: "))
-a = int(input("Enter the value of a: "))
-b = int(input("Enter the value of b: "))
+    if choice == 1:
+        print("The result is:", add(a,b))
+            
+    elif choice == 2: 
+        print("The result is:", subtract(a,b))
 
-if choice == 1:
-    print("The result is:", add(a,b))
-        
-elif choice == 2: 
-    print("The result is:", subtract(a,b))
+    elif choice == 3:
+        print("The result is:", multiply(a,b))
 
-elif choice == 3:
-    print("The result is:", multiply(a,b))
+    elif choice == 4:
+        print("The result is:", divide(a,b))
 
-elif choice == 4:
-    print("The result is:", divide(a,b))
+    while True:
+        ask = input("Do you want to continue using my calculator? Yes/No: ")
+        if ask.lower() == "yes":
+            break
+        elif ask.lower() == "no":
+            break
+        else:
+            print("Please enter either yes or no.")
+            continue
+
         
 
         
