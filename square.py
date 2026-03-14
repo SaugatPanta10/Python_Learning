13db2eb48a342a7aecae5170e5360f5cd0f22f9d
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-14 21:55:08 +0545

    This is program to calculate the square of a number

---
+++ square.py
@@ -0,0 +1,18 @@
+while True: 
+    while True:
+        try:
+            a = int(input("Enter the value of a: "))
+            break
+        except ValueError:
+            print("Invalid Input ! please enter numbers only.")
+       
+    print(f"The square of a is {a*a} ")
+    choice = input("Please enter if you want to continue the program: Yes/No?: ")
+    if choice.lower() == "yes":
+        continue
+    elif choice.lower() == "no":
+        print("Thank you for choosing us. Bye!")
+        break
+
+    else:
+        print("Please enter either yes or no.")
