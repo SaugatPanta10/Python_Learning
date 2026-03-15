17571197b93f5773df069eefc302903696068317
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-15 21:44:56 +0545

    now the second calculator is fully logical and applicable

--- calculator2.py
+++ calculator2.py
@@ -1,25 +1,48 @@
 
 print("Welcome to our calculator")
 while True:
-    try:
-        a = int(input("Enter the first number:"))
-        break
-    except ValueError:
-        print("Invalid Input! Enter the integers only.")
+    while True:
+        try:
+            a = int(input("Enter the first number:"))
+            break
+        except ValueError:
+            print("Invalid Input! Enter the integers only.")
 
-    op = input("Enter the operator (+,-,*,/):")
-    b = int(input("Enter the second number:"))
+    while True:
+        op = input("Enter the operator (+,-,*,/):")
+        if op not in ("+", "-", "*", "/"):
+            print("Please enter the operators only.")
+            continue
+        else:
+            break
+    while True:
+        try:
+            b = int(input("Enter the second number:"))
+            break
+        except ValueError:
+            print("Invalid Input! Enter the integers only.")
 
     if op == "+":
-        print(f"When added {a} with {b}, the result is {a+b}")
+        print(f"{a} {op} {b} gives {a+b}")
 
     elif op == "-":
-        print(f"When subtracted {a} with {b}, the result is {a-b}")
+        print(f"{a} {op} {b} gives {a-b}")
 
     elif op == "*":
-        print(f"When multiplied {a} with {b}, the result is {a*b}")
+        print(f"{a} {op} {b} gives {a*b}")
 
     elif op == "/":
-        print(f"When divided {a} with {b}, the result is {a/b}")
+        print(f"{a} {op} {b} gives {a/b}")
 
+    while True:
+        choice = input("Enter if you want to continue using our calculator, Yes/No ?: ")
+        if choice.lower() == "yes":
+            break
+        
+        elif choice.lower() == "no":
+            print("Thank you for using our calculator. Bye!")
+            exit()
 
+        else:
+            print("Please enter either yes or no.")
+            continue
