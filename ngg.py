84f9095000d52eec41c4edac492f61ccbebe54e0
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-15 22:59:03 +0545

    new number guessing game

--- ngg.py
+++ ngg.py
@@ -1,34 +1,34 @@
 import random 
 while True:
-    print("Welcome to the Number Guessing Game!")
-    sn = random.randint(1,20)
-    print("I have choosen a number between 1 and 20. Lets see if you can guess.")
+    print("Welcome to my game")
+    sn = random.randint(1,10)
     attempt = 0
+    print("I have choosen a number from 1 to 10. Let's see if you can guess.")
     while True:
         try:
-            guess = int(input("Try to guess a number: "))
+            guess = int (input("Guess a number: "))
         except ValueError:
             print("Invalid Input! Please enter numbers only.")
             continue
         attempt += 1
-        if guess<sn:
-            print("The number you guess is lower. Try guessing higher number.")
+        if guess > sn:
+            print("Your guess is bigger than the actual number. Please guess smaller number.")
             continue
-        elif guess>sn:
-            print("The number you guess is higher. Try guessing lower number.")
+        if guess < sn: 
+            print("Your guess is smaller than the actual number. Please guess higher number.")
             continue
         else:
-            print(f"Congratulations. Your guess is correct in {attempt} attempts.")
+            print(f"{guess} is correct. You guessed in {attempt} attempts.")
+            break
+    while True:
+        choice = input("Do you want to continue playing the game ? Yes/No: ").strip().lower()
+        if choice == "yes":
             break
 
-    while True:
-        choice = input("Do you want to continue playing our game? Yes/No: ")
-        if choice.lower() == "yes":
-            break
-        elif choice.lower()== "no":
+        elif choice == "no":
             print("Thank you for playing our game. Please come back soon.")
             exit()
+        
         else:
-            print("Please enter either yes or no!")
+            print("Please enter either yes or no.")
             continue
-
