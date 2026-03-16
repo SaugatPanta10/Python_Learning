a5b4f8236095bd9466ace578c9713e78b13434fe
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-16 20:37:28 +0545

    some improvements done

--- banking.py
+++ banking.py
@@ -1,27 +1,46 @@
-print("Welcome to Nabil Bank")
-print("1. Check Balance")
-print("2. Deposit Money")
-print("3. Withdraw Money")
-print("4. Exit")
+while True:
+    print("Welcome to Nabil Bank")
+    print("1. Check Balance")
+    print("2. Deposit Money")
+    print("3. Withdraw Money")
+    print("4. Exit")
 
-balance = 1000
+    balance = 1000
+    while True:
+        choice = (input("Enter the option above 1/2/3/4 : "))
+        if choice not in ("1", "2", "3", "4"):
+            print("Invalid input! Please enter either 1 or 2 or 3 or 4.")
+            continue
 
-choice = (input("Enter the option above 1/2/3/4 : "))
-if choice == "1":
-    print(f" Your current balance is {balance}.")
+        if choice in ("1", "2", "3", "4"):
+            if choice == "1":
+                print(f" Your current balance is {balance}.")
+                break
 
-if choice == "2":
-    dep = int(input("Enter how much of money you would like to deposit: "))
-    balance = balance + dep
-    print(f" Your current balance is {balance}.")
-    
-if choice == "3":
-    wit = int(input("Enter the amount that you want to withdraw: "))
-    balance = balance - wit
-    print(f" Your current balance is {balance}.")
+            elif choice == "2":
+                dep = int(input("Enter how much of money you would like to deposit: "))
+                balance = balance + dep
+                print(f" Your current balance is {balance}.")
+                break
+                
+            elif choice == "3":
+                wit = int(input("Enter the amount that you want to withdraw: "))
+                balance = balance - wit
+                print(f" Your current balance is {balance}.")
+                break
 
-if choice == "4":
-    print("Thank you for using our bank. Bye")
-    exit()
+            elif choice == "4":
+                print("Thank you for using our bank. Bye")
+                exit()
 
+    while True:
+        ask = input("Do you want to continue our banking service? Yes/No: ").strip().lower()
+        if ask == "yes":
+            break
 
+        elif ask == "no":
+            print("Thank you for using our bank. Bye!")
+            exit()
+
+        else:
+            print("Invalid input! Please enter either yes or no. ")            
