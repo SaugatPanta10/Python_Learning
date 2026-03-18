4eb18a93b196e75dc494d6d5b9394eed00418611
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-18 20:56:23 +0545

    practicing working with menus

---
+++ MenuProductss.py
@@ -0,0 +1,57 @@
+menu = """
+        -----------------
+        1. Add Products
+        2. Show all products
+        3. Search products by name 
+        4. Exit
+        -----------------
+"""
+products = []
+
+while True:
+    print(menu)
+    user_choice = (input("Enter a choice (1/2/3/4): "))
+
+    if user_choice == "1":
+        print("-------Enter product details--------")
+        pr = input("Enter the name of the product that you want to add: ")
+        Price = float(input("Enter the price in npr: "))
+        des = input("Describe your product: ")
+
+        new_products = {
+            "Name": pr,
+            "Price": Price,
+            "Description": des
+        }
+
+        products.append(new_products)
+
+        print("-------------Product added successfully------------")
+
+    elif user_choice == "2":
+        print("--------Product List---------")
+        for i in products:
+            print(f"""
+                name: {i["Name"]}
+                Price: {i["Price"]},
+                Description: {i["Description"]}
+                """)
+
+    elif user_choice == "3":
+        print("-------Product Search----------")
+
+        search_name = input("Enter the product name that you want to search: ").strip().lower()
+        if search_name in products["Name"].strip().lower():
+            print("-------Product found---------")
+            print(f"""
+                    "Name": {products["Name"]},
+                    "Price":{products["Price"]},
+                    "Description":{products["Description"]}
+                """)
+            
+    elif user_choice == "4":
+        print("Thank you for using our services.")
+        break
+
+    else:
+        print("Please choose either 1 or 2 or 3 or 4.")
