418bec15c0e7842006d02d1582b5f29b2af4d590
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-18 20:00:56 +0545

    another dictionary example

---
+++ dict2.py
@@ -0,0 +1,11 @@
+dict2 = {
+    "name": "Saugat", 
+    "Age": 21, 
+    "Post": "Data Analyst",
+    "Salary": 60000
+}
+
+print(f"Old Salary: {dict2["Salary"]}")
+
+dict2["Salary"]= 80000
+print(f"New Salary: {dict2["Salary"]}")
