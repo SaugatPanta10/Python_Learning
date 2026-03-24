0cfb9b03d1cc164a53031df3bbcf838f4510f935
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-24 22:06:28 +0545

    now, the program to calculate the age is running

--- birthday.py
+++ birthday.py
@@ -4,6 +4,5 @@ from dateutil import relativedelta
 birthdate = input("Enter your birthday (yyyy-mm-dd): ")
 birthdate = datetime.strptime(birthdate, "%Y-%m-%d").date()
 today = datetime.today().date()
-diff = relativedelta(today, birthdate)
-age = birthdate-today
-print(age)
+diff = relativedelta.relativedelta(today, birthdate)
+print(f"Your age is {diff.years} years, {diff.months} months, {diff.days} days")
